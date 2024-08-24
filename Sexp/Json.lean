44a4350b3a81647aa84da15759388f10658b11cc
Quinn Dougherty <quinnd@riseup.net>
2024-08-24 14:47:05 -0700

    deriving instance Lean.FromJson for Sexp

--- Sexp/Json.lean
+++ Sexp/Json.lean
@@ -2,6 +2,7 @@ import Sexp.Basic
 import Lean
 
 deriving instance Lean.ToJson for Sexp
+deriving instance Lean.FromJson for Sexp
 
 def Json.write (fh : IO.FS.Handle) (j : Lean.Json) : IO Unit := do
   fh.putStrLn j.compress
